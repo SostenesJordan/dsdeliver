4019a84fa46b7c70b7d5867608f74631c2e89afa
jordan <sostenesj60@gmail.com>
2021-01-08 14:28:33 -0300

    project created

---
+++ Backend/src/main/java/com/devjordan/appdeliv/AppdelivApplication.java
@@ -0,0 +1,13 @@
+package com.devjordan.appdeliv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdelivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdelivApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/devjordan/appdeliv/AppdelivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjordan.appdeliv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdelivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
